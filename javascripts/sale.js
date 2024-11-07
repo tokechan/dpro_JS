6d761f7ea6a8c61a345379f387984ae3c02dcc05
toketime <flatoke@gmail.com>
2024-11-07 21:44:34 +0900

    Initial commit

---
+++ javascripts/sale.js
@@ -0,0 +1,8 @@
+const priceElement = document.getElementById("product");
+const numberElement = document.getElementById("number");
+
+function calc() {
+    const price = parseInt(priceElement.value);
+    const number = parseInt(numberElement.value);
+    window.alert(`${price}円が${number}個。小計は${price * number}円です。`);
+}
---
+++ javascripts/script.js
@@ -0,0 +1,3 @@
+let text = `こんにちは。
+今日はいい天気ですね。`;
+console.log(text);
