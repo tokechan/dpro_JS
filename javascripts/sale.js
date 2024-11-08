76d7ee16857e2049d3a7f4cff8be29d0c4a96dcc
toketime <flatoke@gmail.com>
2024-11-08 23:28:37 +0900

    cal

--- javascripts/sale.js
+++ javascripts/sale.js
@@ -1,8 +1,26 @@
 const priceElement = document.getElementById("product");
 const numberElement = document.getElementById("number");
+let purchases = [];
+
+function add() {
+    const price = parseInt(priceElement.value);
+    const number = parseInt(numberElement.value);
+    const purchase = {
+        price: price,
+        number: number,
+    };
+    purchases.push(purchase);
+}
+
 
 function calc() {
-  const price = parseInt(priceElement.value);
-  const number = parseInt(numberElement.value);
-  window.alert(`${price}円が${number}個。小計は${price * number}円です`);
+    let sum = 0;
+    for (let index = 0; index < purchases.length; index++) {
+        sum += purchases[index].price * purchases[index].number;
+    }
+    window.alert(`合計は${sum}円です`);
+    purchases = [];
+    priceElement.value= "";
+    numberElement.value= "";
 }
+
