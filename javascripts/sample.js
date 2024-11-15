76aa250c0fd6f7e7c5dbc969e0844d7e60a082cf
toketime <flatoke@gmail.com>
2024-11-15 09:03:20 +0900

    validation

--- javascripts/sample.js
+++ javascripts/sample.js
@@ -1,6 +1,28 @@
-function getElement() {
-    const element = document.getElementById("title");
-    element.innerText = "idで取得した要素を変更しました！"
+function addElement() {
+    const parentElement = document.getElementsByTagName("div")[0];
+    const target = document.getElementsByTagName("h1")[0];
+    parentElement.removeChild(target);
 };
 
-window.onload =getElement;
+window.onload = addElement;
+
+
+function addLink() {
+    const element = document.createElement("a");
+    element.setAttribute("href", "https://apple.com");
+    element.innerText ="What is this?";
+    const parentElement = document.getElementsByTagName("div")[0];
+    parentElement.appendChild(element);
+};
+
+
+function removeLink() {
+    const link = document.getElementsByTagName("a")[0];
+    link.removeAttribute("href");
+}
+
+
+window.onload = function() {
+    addLink();
+    removeLink();
+}
