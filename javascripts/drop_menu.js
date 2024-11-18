215f335424a7eadc32d659900d34f7627c152dbd
toketime <flatoke@gmail.com>
2024-11-19 01:02:11 +0900

    ssh setting

--- javascripts/drop_menu.js
+++ javascripts/drop_menu.js
@@ -10,10 +10,10 @@ $(function() {
     dropMenu();
 });
 
-// $(function() {
-//     const button = $("#drop_menu_btn");
-//     const menu = $("#drop_menu");
-//     button.click(function(){
-//       menu.toggle("500", "swing")
-//     });
-//   })
+$(function() {
+    const button = $("#drop_menu_btn");
+    const menu = $("#drop_menu");
+    button.click(function(){
+      menu.toggle("500", "swing")
+    });
+  })
